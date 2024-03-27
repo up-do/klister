45802eccdc350db767741fe42257ceff41993ffc
Phil de Joux <philderbeast@gmail.com>
2024-03-27 08:42:41 -0400

    Bump resolver to lts-20.26

--- project-versions.mk
+++ project-versions.mk
@@ -1,11 +1,11 @@
 # Versions of GHC and stackage resolver, the ones we're on and the next ones
 # we're upgrading to.
 GHC_VERSION ?= 9.2.8
-STACKAGE_VERSION ?= lts-20.25
+STACKAGE_VERSION ?= lts-20.26
 
 # For the upgrade, pick a matching pair of ghc-version and stack resolver.
 GHC_UPGRADE ?= 9.2.8
-STACKAGE_UPGRADE ?= lts-20.25
+STACKAGE_UPGRADE ?= lts-20.26
 
 # Imports can be relative to the project or relative to importing file.
 # ImportRelative works with cabal-3.10 and is the default.
